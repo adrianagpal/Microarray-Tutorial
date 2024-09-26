49f78aaf3dc703bb3e09bab098eb3e750b30cc31
Adriana Gonzalez <adriinter@hotmail.com>
2024-09-26 17:02:01 +0200

    Add file

---
+++ instructions.R
@@ -0,0 +1,10 @@
+#Updating R & RStudio
+https://www.youtube.com/watch?v=P11imusqX0U
+
+# Install Bioconductor
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install()
+
+# Install Git in RStudio
+http://destio.us.es/calvo/asignaturas/ge_esco/tutorialusargitgithubrstudio/UsarGitGithubconRStudio.html
